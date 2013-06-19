a2f8a27bddd4d2d4a68b23309080cea30c734089
Ankoor Patel <ankoor.p@gmail.com>
2013-06-19 21:11:24 +0100

    add functionality for just the N line

--- routemaster.rb
+++ routemaster.rb
@@ -5,11 +5,11 @@ debug = false
 binding.pry if debug
 
 def say(message)
-  puts message if debug
+  puts message if $debug
 end
 
 N = {-1 => '8th', 0 => 'Union Square',  1 => '23rd',  2 => '28th',  3 => '34th',  4 => 'Times Square'}
-L = {-2 => '8th', -1 => '6th', 0 => 'Union Square',  1 => '3rd'  2 => '1st'}
+L = {-2 => '8th', -1 => '6th', 0 => 'Union Square',  1 => '3rd',  2 => '1st'}
 Six = {-1 => 'Astor Place', 0 => 'Union Square',  1 => '23rd',  2 => '28th',  3 => '33rd',  4 => 'Grand Central'}
 
 
@@ -22,4 +22,8 @@ puts "And, where are you travelling to?"
 destination = gets.chomp
 # Check if they typed an existing station
 
-N.detect
+start_number = N.key(origin)
+end_number = N.key(destination)
+
+number_of_stops = end_number - start_number
+puts "You will travel #{number_of_stops.abs} number of stops"
